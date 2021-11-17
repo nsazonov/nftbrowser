886f57a570da48585c4c6981f0256f5976db9bb6
Nikolay Sazonov <nsazonov@fb.com>
2021-11-16 18:37:04 -0800

    Implement load more

--- NFTBrowser/Logger.swift
+++ NFTBrowser/Logger.swift
@@ -5,5 +5,5 @@ let subsystem = "com.nikolaysazonov"
 
 struct Log {
   static let table = OSLog(subsystem: subsystem, category: "table")
-  static let Client = OSLog(subsystem: subsystem, category: "client")
+  static let client = OSLog(subsystem: subsystem, category: "client")
 }
