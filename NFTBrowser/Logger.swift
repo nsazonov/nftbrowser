4d945d4c01e1db6cc8cd31bda8ca5a6d8710b067
Nikolay Sazonov <nsazonov@fb.com>
2021-11-23 13:32:46 -0800

    Implement image caching

--- NFTBrowser/Logger.swift
+++ NFTBrowser/Logger.swift
@@ -4,6 +4,7 @@ import os
 let subsystem = "com.nikolaysazonov"
 
 struct Log {
-  static let table = OSLog(subsystem: subsystem, category: "table")
-  static let client = OSLog(subsystem: subsystem, category: "client")
+    static let table = OSLog(subsystem: subsystem, category: "table")
+    static let client = OSLog(subsystem: subsystem, category: "client")
+    static let imageCache = OSLog(subsystem: subsystem, category: "imageCache")
 }
